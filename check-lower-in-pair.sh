3eb6ed14aaba88842139f3076e54c0907250dc36
Lukas Koch <lukas.koch@mailbox.org>
2022-07-12 22:28:30 +0200

    Use HTTP for extra sensors.

--- check-lower-in-pair.sh
+++ check-lower-in-pair.sh
@@ -4,5 +4,18 @@ HOUR=$(date +%H)
 OTHER=$(($HOUR + 1 - 2 * ($HOUR % 2)))
 NOW_PRICE=$(grep $HOUR:00:00 today.dat | cut -f 2 -d,)
 OTHER_PRICE=$(grep $OTHER:00:00 today.dat | cut -f 2 -d,)
-echo $NOW_PRICE $OTHER_PRICE
-echo "$NOW_PRICE < $OTHER_PRICE" | bc >lower_in_pair
+
+read TOKEN <ha-token
+
+if [[ $NOW_PRICE -lt $OTHER_PRICE ]]
+then
+    curl -X POST -H "Authorization: Bearer $TOKEN" \
+        -H "Content-Type: application/json" \
+        -d '{"state": "on", "attributes": {"friendly_name": "Electricity Price lower in pair"}}' \
+        http://localhost:8123/api/states/binary_sensor.electricity_price_lower_in_pair
+else
+    curl -X POST -H "Authorization: Bearer $TOKEN" \
+        -H "Content-Type: application/json" \
+        -d '{"state": "off", "attributes": {"friendly_name": "Electricity Price lower in pair"}}' \
+        http://localhost:8123/api/states/binary_sensor.electricity_price_lower_in_pair
+fi
