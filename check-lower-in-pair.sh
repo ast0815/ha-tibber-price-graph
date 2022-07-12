e606183f7df5b64f93efac765950a302fc6eb04e
Lukas Koch <lukas.koch@mailbox.org>
2022-07-13 01:18:27 +0200

    Fix zero padding in other hour.

--- check-lower-in-pair.sh
+++ check-lower-in-pair.sh
@@ -2,6 +2,8 @@
 
 HOUR=$(date +%H)
 OTHER=$(($HOUR + 1 - 2 * ($HOUR % 2)))
+# Fix zero padding
+printf -v OTHER %02g $OTHER
 NOW_PRICE=$(grep $HOUR:00:00 today.dat | cut -f 2 -d,)
 OTHER_PRICE=$(grep $OTHER:00:00 today.dat | cut -f 2 -d,)
 
