eb9db11e8f2cc9bb334ae354abf6d3ec0ccb2003
Lukas Koch <lukas.koch@mailbox.org>
2022-07-12 23:02:05 +0200

    Draw final data point horizontal line.

--- make-graph.sh
+++ make-graph.sh
@@ -12,9 +12,13 @@ https://api.tibber.com/v1-beta/gql >data.json
 
 # Prepare csv file with today's prices
 <data.json jq  -r '.data.viewer.homes[0].currentSubscription.priceInfo.today[] | [.startsAt[11:19], .total] | @csv' >today.dat
+# Duplicate last data point so it is drawn
+tail -n1 today.dat | sed -e 's/00/59/' >>today.dat
 
 # Prepare csv file with tomorrow's prices
 <data.json jq  -r '.data.viewer.homes[0].currentSubscription.priceInfo.tomorrow[] | [.startsAt[11:19], .total] | @csv' >tomorrow.dat
+# Duplicate last data point so it is drawn
+tail -n1 tomorrow.dat | sed -e 's/00/59/' >>tomorrow.dat
 
 # Prepare vertical line plot of current time
 NOW=$(date +%H:%M)
