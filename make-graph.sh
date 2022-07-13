daccba682549dc56e4ce33b449193456327ff993
Lukas Koch <lukas.koch@mailbox.org>
2022-07-13 21:42:55 +0200

    Still create a new plot even if the data fetching failed.

--- make-graph.sh
+++ make-graph.sh
@@ -3,22 +3,23 @@
 read TOKEN <token
 
 # Get data from Tibber API
-curl \
--H "Authorization: Bearer $TOKEN" \
--H "Content-Type: application/json" \
--X POST \
--d  '{ "query": "{viewer{homes{currentSubscription{priceInfo{today{startsAt total} tomorrow{startsAt total}}}}}}"}' \
-https://api.tibber.com/v1-beta/gql >data.json
+if curl \
+    -H "Authorization: Bearer $TOKEN" \
+    -H "Content-Type: application/json" \
+    -X POST \
+    -d  '{ "query": "{viewer{homes{currentSubscription{priceInfo{today{startsAt total} tomorrow{startsAt total}}}}}}"}' \
+    https://api.tibber.com/v1-beta/gql >data.json
+then
+    # Prepare csv file with today's prices
+    <data.json jq  -r '.data.viewer.homes[0].currentSubscription.priceInfo.today[] | [.startsAt[11:19], .total] | @csv' >today.dat
+    # Duplicate last data point so it is drawn
+    tail -n1 today.dat | sed -e 's/00/59/' >>today.dat
 
-# Prepare csv file with today's prices
-<data.json jq  -r '.data.viewer.homes[0].currentSubscription.priceInfo.today[] | [.startsAt[11:19], .total] | @csv' >today.dat
-# Duplicate last data point so it is drawn
-tail -n1 today.dat | sed -e 's/00/59/' >>today.dat
-
-# Prepare csv file with tomorrow's prices
-<data.json jq  -r '.data.viewer.homes[0].currentSubscription.priceInfo.tomorrow[] | [.startsAt[11:19], .total] | @csv' >tomorrow.dat
-# Duplicate last data point so it is drawn
-tail -n1 tomorrow.dat | sed -e 's/00/59/' >>tomorrow.dat
+    # Prepare csv file with tomorrow's prices
+    <data.json jq  -r '.data.viewer.homes[0].currentSubscription.priceInfo.tomorrow[] | [.startsAt[11:19], .total] | @csv' >tomorrow.dat
+    # Duplicate last data point so it is drawn
+    tail -n1 tomorrow.dat | sed -e 's/00/59/' >>tomorrow.dat
+fi
 
 # Prepare vertical line plot of current time
 NOW=$(date +%H:%M)
